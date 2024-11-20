e6eddc9e1149964a19bb17039e2de4a4bea07792
tanisha1527 <vermatanisha620@gmail.com>
2024-11-21 02:27:59 +0530

    add another file

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
+import React from "react"
+import "./Weather.css"
 
 const Weather = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+  return <div>
+
+  </div>
 }
 
 export default Weather
