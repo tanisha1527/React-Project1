f5a056666895fb3fcddee8322ae82add753cd422
tanisha1527 <vermatanisha620@gmail.com>
2024-11-21 02:36:04 +0530

    updated some files

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -2,9 +2,14 @@ import React from "react"
 import "./Weather.css"
 
 const Weather = () => {
-  return <div>
-
+  return (
+     <div className="weather">
+        <div className="search-bar">
+         <input type="text" placeholder="Search" />
+         <img src="" alt="" />
+        </div> 
   </div>
+  )
 }
 
 export default Weather
