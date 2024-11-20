e5e4e5dbbfc1f55ee2388b88ebf477a115b5b892
tanisha1527 <vermatanisha620@gmail.com>
2024-11-21 03:10:02 +0530

    update files

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,13 +1,25 @@
 import React from "react"
 import "./Weather.css"
+import search_icon from '../assets/search.png'
+import clear_icon from '../assets/clear.png'
+import cloud_icon from '../assets/cloud.png'
+import drizzle_icon from '../assets/drizzle.png'
+import rain_icon from '../assets/rain.png'
+import snow_icon from '../assets/snow.png'
+import wind_icon from '../assets/wind.png'
+import humidity_icon from '../assets/humidity.png'
+
 
 const Weather = () => {
   return (
      <div className="weather">
         <div className="search-bar">
          <input type="text" placeholder="Search" />
-         <img src="" alt="" />
+         <img src={search_icon} alt="" />
         </div> 
+        <img src={clear_icon} alt="" className="weather-icon" />
+        <p className="temp">16°c</p>
+        <p className="location">Delhi</p>
   </div>
   )
 }
