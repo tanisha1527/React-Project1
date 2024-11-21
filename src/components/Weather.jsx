fd93ca1a2d573235e33926ed683d0a27bb12991a
tanisha1527 <vermatanisha620@gmail.com>
2024-11-21 15:19:13 +0530

    change file location

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,43 +1,57 @@
-import React from "react"
-import "./Weather.css"
-import search_icon from '../assets/search.png'
-import clear_icon from '../assets/clear.png'
-import cloud_icon from '../assets/cloud.png'
-import drizzle_icon from '../assets/drizzle.png'
-import rain_icon from '../assets/rain.png'
-import snow_icon from '../assets/snow.png'
-import wind_icon from '../assets/wind.png'
-import humidity_icon from '../assets/humidity.png'
-
+import React, { useEffect } from "react";
+import "./Weather.css";
+import search_icon from "../assets/search.png";
+import clear_icon from "../assets/clear.png";
+import cloud_icon from "../assets/cloud.png";
+import drizzle_icon from "../assets/drizzle.png";
+import rain_icon from "../assets/rain.png";
+import snow_icon from "../assets/snow.png";
+import wind_icon from "../assets/wind.png";
+import humidity_icon from "../assets/humidity.png";
 
 const Weather = () => {
-  return (
-     <div className="weather">
-        <div className="search-bar">
-         <input type="text" placeholder="Search" />
-         <img src={search_icon} alt="" />
-        </div> 
-        <img src={clear_icon} alt="" className="weather-icon" />
-        <p className="temp">16°c</p>
-        <p className="location">Delhi</p>
-        <div className="weather-data">
-          <div className="column">
-          <img src={humidity_icon} alt="" />
-           <div>
-              <p>91 %</p>
-              <span>Humidity</span>
-           </div>
-          </div>
-          <div className="column">
-          <img src={wind_icon} alt="" />
-           <div>
-              <p>3.0 km/h</p>
-              <span>Wind Speed</span>
-           </div>
-          </div>
-    </div>
-  </div>
-  )
-}
+  
+   const search = async (city) => {
+    try {
+         const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${import.meta.env.VITE_APP_ID}`;
 
-export default Weather
+         const response = await fetch(url);
+         const data = await response.json();
+         console.log(data);
+      } catch (error) {}
+  };
+
+  useEffect(() => {
+    search("Delhi");
+  }, []);
+
+  return (
+    <div className="weather">
+      <div className="search-bar">
+        <input type="text" placeholder="Search" />
+        <img src={search_icon} alt="" />
+      </div>
+      <img src={clear_icon} alt="" className="weather-icon" />
+      <p className="temp">16°c</p>
+      <p className="location">Delhi</p>
+      <div className="weather-data">
+        <div className="column">
+          <img src={humidity_icon} alt="" />
+          <div>
+            <p>91 %</p>
+            <span>Humidity</span>
+          </div>
+        </div>
+        <div className="column">
+          <img src={wind_icon} alt="" />
+          <div>
+            <p>3.0 km/h</p>
+            <span>Wind Speed</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Weather;
