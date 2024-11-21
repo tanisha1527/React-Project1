ca1519acdc177e2e6ff44537ca32149f8140d76e
tanisha1527 <vermatanisha620@gmail.com>
2024-11-21 23:23:51 +0530

    Add final changes

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./Weather.css";
 import search_icon from "../assets/search.png";
 import clear_icon from "../assets/clear.png";
@@ -10,42 +10,72 @@ import wind_icon from "../assets/wind.png";
 import humidity_icon from "../assets/humidity.png";
 
 const Weather = () => {
+   
+   const inputRef = useRef()
+   const [weatherData, setWeatherData] = useState(false);
+
+   const allIcons = {
+         "01d": clear_icon,
+         "01n": clear_icon,
+         "02d": cloud_icon,
+         "02n": cloud_icon,
+         "03d": cloud_icon,
+         "03n": cloud_icon,
+         "04d": drizzle_icon,
+         "04n": drizzle_icon,
+         "09d": rain_icon,
+         "09n": rain_icon,
+         "10d": rain_icon,
+         "10n": rain_icon,
+         "13d": snow_icon,
+         "13n": snow_icon,
+   }
   
    const search = async (city) => {
     try {
-         const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${import.meta.env.VITE_APP_ID}`;
+         const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.VITE_APP_ID}`;
 
          const response = await fetch(url);
          const data = await response.json();
          console.log(data);
-      } catch (error) {}
-  };
+         const icon = allIcons[data.weather[0].icon] || clear_icon;
+         setWeatherData({
+            humidity: data.main.humidity,
+            windSpeed: data.wind.speed,
+            temprature: Math.floor(data.main.temp),
+            location: data.name,
+            icon:icon
+         })
+      } catch (error) {
+
+      }
+  }
 
   useEffect(() => {
-    search("Delhi");
+    search("New York");
   }, []);
 
   return (
     <div className="weather">
       <div className="search-bar">
-        <input type="text" placeholder="Search" />
-        <img src={search_icon} alt="" />
+        <input ref={inputRef} type="text" placeholder="Search" />
+        <img src={search_icon} alt="" onClick={()=>search(inputRef.current.value)} />
       </div>
-      <img src={clear_icon} alt="" className="weather-icon" />
-      <p className="temp">16°c</p>
-      <p className="location">Delhi</p>
+      <img src={weatherData.icon} alt="" className="weather-icon" />
+      <p className="temp">{weatherData.temprature}°c</p>
+      <p className="location">{weatherData.location}</p>
       <div className="weather-data">
         <div className="column">
           <img src={humidity_icon} alt="" />
           <div>
-            <p>91 %</p>
+            <p>{weatherData.humidity} %</p>
             <span>Humidity</span>
           </div>
         </div>
         <div className="column">
           <img src={wind_icon} alt="" />
           <div>
-            <p>3.0 km/h</p>
+            <p>{weatherData.windSpeed} km/h</p>
             <span>Wind Speed</span>
           </div>
         </div>
